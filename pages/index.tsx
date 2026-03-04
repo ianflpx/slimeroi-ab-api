07620bf024f1cf86854f93b72d1744aade3d16a9
Macaco Prego <89818913+ianflpx@users.noreply.github.com>
2026-03-04 12:47:22 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontFamily: 'system-ui, sans-serif'
+        }}>
+            <h1>Edge Traffic Controller</h1>
+        </div>
+    );
+}
